83d537286686cf0a2aeb9b6cc2a727b11ca06613
Nur Sasongko <kokonur1020@gmail.com>
2023-07-17 17:18:10 +0700

    feat: solve me first

---
+++ problem-solving/basic/solve-me-first/solve-me-first.js
@@ -0,0 +1,6 @@
+function solveMeFirst(a, b) {
+    return a + b;
+}
+var a = 2;
+var b = 3;
+console.log(solveMeFirst(a, b));
---
+++ problem-solving/basic/solve-me-first/solve-me-first.ts
@@ -0,0 +1,8 @@
+function solveMeFirst(a: number, b: number): number {
+  return a + b;
+}
+
+const a: number = 2;
+const b: number = 3;
+
+console.log(solveMeFirst(a, b));
